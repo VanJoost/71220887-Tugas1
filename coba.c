8cd109656e4569865e748fac5ea2e2017824b71b
VanJoost <117902025+VanJoost@users.noreply.github.com>
2023-09-21 18:39:40 +0700

    Add files via upload

---
+++ coba.c
@@ -0,0 +1,10 @@
+# Program percabangan sederhana dalam Python
+
+# Meminta input dari pengguna
+umur = int(input("Masukkan umur Anda: "))
+
+# Melakukan percabangan
+if umur < 18:
+    print("Anda masih di bawah umur.")
+else:
+    print("Anda sudah dewasa.")
